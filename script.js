7976fef5dc0aef0354aebede9cb77cdaff86ac41
samjhopkins9 <samjhopkins9@gmail.com>
2024-05-13 11:36:24 -0400

    Add source notebook, change homepage to projects

--- script.js
+++ script.js
@@ -25,13 +25,14 @@ let Course = (certificate, coursename, coursesite) => {
     }
 }
 
-let Project = (projlink, projname, projdesc, projid, projseas) => {
+let Project = (projlink, projname, projdesc, projid, projseas, addLinks = null) => {
     return {
         projlink,
         projname,
         projdesc,
         projid,
-        projseas
+        projseas,
+        addLinks
     }
 }
 
@@ -99,7 +100,7 @@ let projects = [
     "Current EDM production/DJing project.", 'redwave', "September 2023 - Present"),
 
     Project("https://samjhopkins9.github.io/SalaryModel_RF/docs/Report.html", "Regression Model for Predicting Salary (R)",
-            "This project uses a dataset downloaded from Kaggle to create a regression model to predict an individual's salary based on their age, years of experience, education level, and gender.", "salarymodel", "February 2024 - Present"),
+            "This project uses a dataset downloaded from Kaggle to create a regression model to predict an individual's salary based on their age, years of experience, education level, and gender.", "salarymodel", "February 2024 - Present", ["https://samjhopkins9.github.io/SalaryModel_RF/src/Project1.html"]),
     Project("https://samjhopkins9.github.io/Character-Classifier/", "Image Classifier for Written Characters (Python)",
             "This project uses mnist data from python's scikit-learn module to create a model which identifies images of written characters a-z and 0-9.", "imagemodel", "March 2024"),
     Project("https://samjhopkins9.github.io/Banknote-Classifier/", "Classifier for Real/Fake Banknotes (Python)",
@@ -108,7 +109,6 @@ let projects = [
     Project("https://samjhopkins9.github.io/Options-Calculator-Market-Newsfeed", "Black-Scholes Options Price Calculator & Market Newsfeed (JavaScript/HTML/CSS)",
             "This desktop webpage displays news, some simple charts, and some basic information pertaining to a volatility of the given stock or ETF using minutely data over the last 2 weeks. It also uses the Black-Scholes formula to calculate the price of short-term options contracts with strike prices near the underlying, at various levels of implied volatility, with time decay measured after a certain amount of minutes, and the underlying price taken at three points: its current price, plus, and minus a specified percentage movement. This was largely created for the purpose of calculating fluctuations in high-volume daily contracts on ETFs.", 'interface', "July 2023"),
     
-    Project('https://www.youtube.com/watch?v=dembujuTyI4', "Spacegoat", "Contemporary jazz and neo-soul band of which I was the drummer during my junior year at Marin Academy. We recorded this video for the Marin Independent Journal in February 2019, and played in the showcase of their selection of Marin County's best high school musicians in April of that same year.", 'space', "August 2018 - June 2019")
     
 ];
 
@@ -158,6 +158,11 @@ function loadHTML(){
         projectshtml += `<li id="${projects[i].projid}"><a href='${projects[i].projlink}' target='_blank'>${projects[i].projname}</a></li>
                             <p>${projects[i].projseas}</p>
                             <p class="jobdesc">${projects[i].projdesc}</p>`;
+        if (projects[i].projname === "Regression Model for Predicting Salary (R)"){
+            for (link of projects[i].addLinks){
+                projectshtml += `<a href="${link}" target="_BLANK">Source Notebook</a><br><br>`;
+            }
+        }
         if (projects[i].projname === "REDWAVE."){
             projectshtml += `<div id="musiclinks"><iframe width="50%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1805557528&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><iframe width="50%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1758975567&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe></div><br><br>`;
         }
