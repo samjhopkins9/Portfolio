18783199b1aa4766166c24e92110ae071cc430e9
samjhopkins9 <samjhopkins9@gmail.com>
2024-11-13 17:46:10 -0500

    Update

--- script.js
+++ script.js
@@ -38,10 +38,8 @@ let Project = (projlink, projname, projdesc, projid, projseas, addLinks = null) 
 
 // document.querySelector("html").style.backgroundImage = "url(background.jpeg)";
 let myname = "Sam Hopkins";
-let degreetype = "BS";
-let degreename = "Data Science & AI";
-let degree2type = "BA";
-let degree2name = "Computer Science";
+let degreetype = "Dual BS";
+let degreename = "Data Science & AI + Computer Science Fundamentals";
 let university = "University of Miami";
 let email = "samjhopkins9@gmail.com";
 let phonenumber = "415-480-9033";
@@ -49,7 +47,7 @@ let linkedin = "https://www.linkedin.com/in/sam-hopkins-6656a6205/";
 let bio = `Lifelong musician with a passion for programming and data analysis. I am skilled in a diverse set of programming languages including Python, R, C++, Java, Bash, JavaScript, HTML and CSS. My interests include data analytics, machine learning, audio software development and sound design.
     <br>
     <br>
-    I was born in Boston, experienced my earliest memories living in Hong Kong, and primarily grew up in Mill Valley, CA, right outside of San Francisco. Now, I am at the University of Miami pursuing a B.S. in Data Science and Artificial Intelligence with minors in Math and Music, and am seeking an internship in Data Analytics for summer of 2024.
+    I was born in Boston, experienced my earliest memories living in Hong Kong, and primarily grew up in Mill Valley, CA, right outside of San Francisco. Now, I am at the University of Miami pursuing a B.S. in Data Science and Artificial Intelligence with minors in Math and Music, and am seeking an internship in Data Analytics for summer of 2025.
     <br>
     I spent my first two and a half years in Miami pursuing a degree in Music Production/Engineering in the Frost School of Music. I decided to change my course after I learned to code via online courses during the summer of 2022 and quickly found myself immersed in it and driven to learn as much as I could. I have realized that the aspects of computer science that appeal to me the most are the data-driven ones. I would be happy in any position where I can gather, inspect, clean, crunch, and interpret data. `;
 
@@ -63,16 +61,16 @@ let jobs = [
         "Bussed tables, stocked items, took phone orders and sat customers in a full-service Mexican restaurant in Mill Valley, CA.",
         "15-25 hours/week, ", "June-August 2020/2021, June-July 2022"),
     Job("Music Producer", "Self-employed", "Ongoing",
-        "Writer and producer of electronic music, working under the name REDWAVE. I have released two 4-song EPs so far this year. I released 4 singles and a 4-song EP under the name Blue Heights in 2020, and before that released 3 songs under the name SMJH.",
+        "Writer and producer of electronic music, working under the name REDWAVE. I have released four 4-5 song EPs this year.",
         "", ""),
     Job("Computer Programmer", "Self-employed", "Ongoing",
-        "Completed several self-directed projects, including: A Black-Scholes-based option price calculator with an attached chart section and market newsfeed, within an html-based UI; a random forest-based regression model to predict salaries; a classifier for written characters; and a classifier of real/fake banknotes.",
+        "Completed several self-directed projects, including: A convolutional neural network for predicting vehicle position in a garage, along with a set of c++ header files for data handling, machine learning and multilayer perceptron in c++; a Black-Scholes-based option price calculator with an attached chart section and market newsfeed, within an html-based UI; a random forest-based regression model to predict salaries; a classifier for written characters; and a classifier of real/fake banknotes.",
         "", "")
 ];
 
 let schools = [
     School("Marin Academy", "San Rafael, CA", "Class of 2020."),
-    School("University of Miami", "Coral Gables, FL", "BS in Data Science & Artificial Intelligence + BA in Computer Science, minors in Math and Music. Founding Father of Zeta Beta Tau Fraternity, Alpha Omega Chapter. ")
+    School("University of Miami", "Coral Gables, FL", "Dual BS in Data Science & Artificial Intelligence + Computer Science Fundamentals, minors in Math and Music. Founding Father of Zeta Beta Tau Fraternity, Alpha Omega Chapter. ")
 ];
 
 let courses = [
@@ -102,20 +100,25 @@ let courses = [
 
 let projects = [
     
+    Project("https://github.com/samjhopkins9/CSI-Prediction-Models", "Convolutional Neural Network to Predict Vehicle Position (Python) + Data Handling, Machine Learning and Multilayer Perceptron Algorithm Design (C++)",
+            "This project uses a dataset containing channel state information from a remote radio unit receiving signals from cars parked in various locations around a garage to predict: a) the position of a vehicle, and b) and whether or not the vehicle is within line of sight from the remote radio unit. The most effective models were convolutional neural networks written in python using tensorflow. This project also contains manually written implementations of linear/logistic regression, KNN, and multilayer perceptron in c++, and a written proof of the gradient update rule in a multilayer perceptron.",
+            "nnpr1", "October - November 2024"),
+    
     Project("https://samjhopkins9.github.io/SalaryModel_RF/docs/Report.html", "Regression Model for Predicting Salary (R)",
-            "This project uses a dataset downloaded from Kaggle to create a regression model to predict an individual's salary based on their age, years of experience, education level, and gender.", "salarymodel", "February 2024 - Present", ["https://samjhopkins9.github.io/SalaryModel_RF/src/Project1.html"]),
+            "This project uses a dataset downloaded from Kaggle to create a regression model to predict an individual's salary based on their age, years of experience, education level, and gender.", "salarymodel", "February - May 2024", ["https://samjhopkins9.github.io/SalaryModel_RF/src/Project1.html"]),
     Project("https://samjhopkins9.github.io/Character-Classifier/", "Image Classifier for Written Characters (Python)",
             "This project uses mnist data from python's scikit-learn module to create a model which identifies images of written characters a-z and 0-9.", "imagemodel", "March 2024"),
     Project("https://samjhopkins9.github.io/Banknote-Classifier/", "Classifier for Real/Fake Banknotes (Python)",
             
             "This project uses .csv data to classify banknotes as real or fake based on several given attributes.", "notemodel", "March 2024"),
     
-    Project('https://whoisredwave.com', 'REDWAVE.',
-    "Current EDM production/DJing project.", 'redwave', "September 2023 - Present"),
+
 
     Project("https://samjhopkins9.github.io/Options-Calculator-Market-Newsfeed", "Black-Scholes Options Price Calculator & Market Newsfeed (JavaScript/HTML/CSS)",
             "This desktop webpage displays news, some simple charts, and some basic information pertaining to a volatility of the given stock or ETF using minutely data over the last 2 weeks. It also uses the Black-Scholes formula to calculate the price of short-term options contracts with strike prices near the underlying, at various levels of implied volatility, with time decay measured after a certain amount of minutes, and the underlying price taken at three points: its current price, plus, and minus a specified percentage movement. This was largely created for the purpose of calculating fluctuations in high-volume daily contracts on ETFs.", 'interface', "July 2023"),
     
+    Project('https://whoisredwave.com', 'REDWAVE.',
+    "Current EDM production/DJing project.", 'redwave', "September 2023 - Present")
     
 ];
 
@@ -123,8 +126,8 @@ function loadHTML(){
     // document.getElementById('headshotp').innerHTML = `<img src="headshot2.jpeg" id="headshot"></img>`;
     document.getElementById('title').innerHTML = myname;
     document.getElementById('name').innerHTML = myname;
-    document.getElementById('description').innerHTML = `${degreetype} in ${degreename} + `;
-    document.getElementById('description').innerHTML += `${degree2type} in ${degree2name} <br>`;
+    document.getElementById('description').innerHTML = `${degreetype} in ${degreename} @ `;
+    // document.getElementById('description').innerHTML += `${degree2type} in ${degree2name} <br>`;
     document.getElementById('description').innerHTML += `University of Miami`;
     document.getElementById('bio').innerHTML = bio;
     
